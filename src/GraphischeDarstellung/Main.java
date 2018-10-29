164942b6fc59001203ac1755f5dcda5636e76cf3
l.hofer <l.hofer@posteo.de>
2018-10-29 15:24:05 +0100

    Grundlegende Benutzeroberlaeche

--- src/GraphischeDarstellung/Main.java
+++ src/GraphischeDarstellung/Main.java
@@ -1,24 +1,44 @@
 package GraphischeDarstellung;
 	
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
+	
+	Stage fenster;
+	
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+	public void start(Stage primaryStage) throws Exception{
+		/*
+		 * erstellt das Hauptfenster
+		 */
+		this.fenster = primaryStage;
+		this.fenster.setTitle("Graphengenerierung");
+		VBox hauptFenster = new VBox();
+		hauptFenster.setSpacing(10);
+		hauptFenster.setPadding(new Insets(8, 8, 8, 8));
+		
+		/*
+		 *erstellt Buttonleiste fuer grundlegende Funktionen 
+		 */
+		HBox buttonLeiste = new HBox();
+		buttonLeiste.setSpacing(15);
+		Button neuerGraphButton = new Button("Neu");
+		Button beendenButton = new Button("Beenden");
+		buttonLeiste.getChildren().addAll(neuerGraphButton, beendenButton);
+		
+		hauptFenster.getChildren().addAll(buttonLeiste);
+		this.fenster.setScene(new Scene(hauptFenster, 400, 400));
+		this.fenster.setResizable(false);
+		this.fenster.show();
+	}	
 	
 	public static void main(String[] args) {
 		launch(args);
